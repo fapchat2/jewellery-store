84f9b4d444af317e98854e91664802d20189b277
fapchat2 <fapchat2@gmail.com>
2022-04-02 21:18:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+
+
+
+function fadeIn(el) {
+  
+	var opacity = 0.01;
+  
+	el.style.display = "block";
+  
+	var timer = setInterval(function() {
+    
+		if(opacity >= 1) {
+			
+			clearInterval(timer);
+		
+		}
+		
+		el.style.opacity = opacity;
+     
+		opacity += opacity * 0.1;
+   
+	}, 10);
+	
+}
+
+function fadeOut(el) {
+  
+	var opacity = 1;
+  
+	var timer = setInterval(function() {
+    
+		if(opacity <= 0.1) {
+		
+			clearInterval(timer);
+			el.style.display = "none";
+	
+		}
+	
+		el.style.opacity = opacity;
+     
+		opacity -= opacity * 0.1;
+   
+	}, 10);
+
+}
+
+
+
+
+
+let buttons = document.querySelectorAll(".true-beauty .buttons button");
+let borderButtons = document.querySelectorAll(
+    ".true-beauty .buttons > div > div"
+);
+let photos = document.querySelectorAll(".true-beauty .photos");
+
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].onmousemove = () => {
+        borderButtons[index].style.display = "none";
+    };
+    buttons[index].onmouseout = () => {
+        if (window.innerWidth > 1800)
+            borderButtons[index].style.display = "block";
+    };
+
+    buttons[index].onclick = () => {
+        // $(".true-beauty .photos:nth-child(1)").fadeIn();
+        photos.forEach(element => {
+            element.style.display = "none";
+        });
+        fadeIn(photos[index]);
+
+        // document.querySelectorAll(".true-beauty .photos1").fadeIn();
+    };
+
+}
+
+
