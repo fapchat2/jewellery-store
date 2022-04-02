80c9e1a4533d778e706bc81a4b6b07d16c633900
fapchat2 <fapchat2@gmail.com>
2022-04-02 21:45:19 +0300

    #prettify

--- js/script.js
+++ js/script.js
@@ -1,52 +1,33 @@
-
-
-
 function fadeIn(el) {
-  
-	var opacity = 0.01;
-  
-	el.style.display = "block";
-  
-	var timer = setInterval(function() {
-    
-		if(opacity >= 1) {
-			
-			clearInterval(timer);
-		
-		}
-		
-		el.style.opacity = opacity;
-     
-		opacity += opacity * 0.1;
-   
-	}, 10);
-	
+    var opacity = 0.01;
+
+    el.style.display = "block";
+
+    var timer = setInterval(function () {
+        if (opacity >= 1) {
+            clearInterval(timer);
+        }
+
+        el.style.opacity = opacity;
+
+        opacity += opacity * 0.1;
+    }, 10);
 }
 
 function fadeOut(el) {
-  
-	var opacity = 1;
-  
-	var timer = setInterval(function() {
-    
-		if(opacity <= 0.1) {
-		
-			clearInterval(timer);
-			el.style.display = "none";
-	
-		}
-	
-		el.style.opacity = opacity;
-     
-		opacity -= opacity * 0.1;
-   
-	}, 10);
+    var opacity = 1;
 
+    var timer = setInterval(function () {
+        if (opacity <= 0.1) {
+            clearInterval(timer);
+            el.style.display = "none";
+        }
+
+        el.style.opacity = opacity;
+
+        opacity -= opacity * 0.1;
+    }, 10);
 }
-
-
-
-
 
 let buttons = document.querySelectorAll(".true-beauty .buttons button");
 let borderButtons = document.querySelectorAll(
@@ -65,14 +46,11 @@ for (let index = 0; index < buttons.length; index++) {
 
     buttons[index].onclick = () => {
         // $(".true-beauty .photos:nth-child(1)").fadeIn();
-        photos.forEach(element => {
+        photos.forEach((element) => {
             element.style.display = "none";
         });
         fadeIn(photos[index]);
 
         // document.querySelectorAll(".true-beauty .photos1").fadeIn();
     };
-
 }
-
-
